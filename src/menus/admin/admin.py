2ff548df6367254a2c3487df8d03b03dd9266138
Mr.Flava <thatelitemaili33t@gmail.com>
2019-06-24 19:45:10 +0300

    changing admin menu

--- src/menus/admin/admin.py
+++ src/menus/admin/admin.py
@@ -5,7 +5,7 @@ from src.local_settings import admin_password
 from src.menus.admin.cars_control import CarsListMenu
 from src.menus.admin.show_customers import ShowCustomers
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from botmanlib.menus.helpers import unknown_command, add_to_db, to_state
+from botmanlib.menus.helpers import unknown_command, add_to_db, to_state, generate_underscore
 from telegram.ext import CommandHandler, CallbackQueryHandler, ConversationHandler, MessageHandler, Filters
 
 
@@ -29,6 +29,7 @@ class AdminMenu(BaseMenu):
         if not add_to_db(user, session=DBSession):
             return self.conv_fallback(user_data)
         user_data['user'] = user
+        _ = user_data['_'] = generate_underscore(user)
         text = update.message.text.replace('/admin', '')
         text = "".join(text.split())
         user_data['password'] = text
@@ -36,7 +37,7 @@ class AdminMenu(BaseMenu):
             admin_keyboard = [[InlineKeyboardButton('О правах администратора', callback_data='about'),
                                InlineKeyboardButton('Данные клиентов', callback_data='show_customers')],
                               [InlineKeyboardButton('Данные пользователей', callback_data='show_users'),
-                               InlineKeyboardButton('Данные машин', callback_data='cars_data')]]
+                               InlineKeyboardButton('Данные машин', callback_data='cars_control')]]
             reply_markup = InlineKeyboardMarkup(admin_keyboard)
             update.message.reply_text('Активирован режим администратора!',
                                       reply_markup=reply_markup)
@@ -48,6 +49,7 @@ class AdminMenu(BaseMenu):
 
     def about_adminmode(self, bot, update, user_data):
         user = user_data['user']
+        _ = user_data['_']
         query = update.callback_query
         self.send_or_edit(user_data, text='Режим администратоатора позволяет просматривать данные пользователей, '
                                           'клиентов и работать с БД-машин', chat_id=user.chat_id)
@@ -55,6 +57,7 @@ class AdminMenu(BaseMenu):
 
     def show_users(self, bot, update, user_data):
         user = user_data['user']
+        _ = user_data['_']
         users = DBSession.query(User).all()
         query = update.callback_query
         self.send_or_edit(user_data, text='Список всех пользователей бота:', chat_id=user.chat_id)
@@ -67,18 +70,6 @@ class AdminMenu(BaseMenu):
                 text=' Имя:{}'.format(name) + 'Юзернейм:{}'.format(username) + 'Активность:{}'.format(active)
                      + 'Дата начала работы с ботом:{}'.format(join_date),
                 chat_id=query.message.chat_id, message_id=query.message.message_id)
-        return self.States.ACTION
-
-    def cars_data(self, bot, update, user_data):
-        user = user_data['user']
-        query = update.callback_query
-        submenu_keyboard = [[InlineKeyboardButton('Показать автомобили', callback_data='show_cars'),
-                             InlineKeyboardButton('Удалить автомобиль', callback_data='delete_cars')],
-                            [InlineKeyboardButton('Изменить описание', callback_data='desc_cars'),
-                             InlineKeyboardButton('Добавить автомобиль', callback_data='add_cars')]]
-        reply_markup = InlineKeyboardMarkup(submenu_keyboard)
-        self.send_or_edit(user_data, text='Выберите операцию:', chat_id=user.chat_id,
-                          reply_markup=reply_markup)
         return self.States.ACTION
 
     def get_handler(self):
